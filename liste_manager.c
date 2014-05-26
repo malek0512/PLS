776a2d5f81c14808612ec84e0959fb5095160af1
Quego <quentinmace@hotmail.com>
2014-05-26 17:23:07 +0200

    Realisation de ajoutEnTete

--- liste_manager.c
+++ liste_manager.c
@@ -1,6 +1,24 @@
 #include "liste_manager.h"
 
-//a faire
+
 void ajoutEnTete(maillon* Tete, int lettre, int frequence){
+
+	// On crée un nouvel élément 
+	maillon* nouvelElement = malloc(sizeof(maillon));
+ 
+	//On assigne la valeur au nouvel élément 
+	nouvelElement->lettre = lettre;
+
+	//On assigne la frequence au nouvel élément 
+	nouvelElement->autre = frequence;
+ 
+	//On assigne l'adresse de l'élément suivant au nouvel élément 
+	nouvelElement->suivant = Tete;
+ 
+	//On retourne la nouvelle liste, i.e. le pointeur sur le premier élément
+	return nouvelElement;
     
 }
+
+
+
