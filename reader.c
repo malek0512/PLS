674140d37c2fc4160358f850fcad6b66c5440518
Malek MAMMAR <malekmammar@gmail.com>
2014-05-26 15:06:13 +0200

    Package liste_manager a remplir

---
+++ reader.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+maillon* readFromFile(char *nom){
+    FILE data;
+    data = fopen(nom, "r+");
+    
+    char c;
+    do while (c != EOF){
+        c = getchar(f);
+        
+    }
+
+}
